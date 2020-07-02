f9ad96f974a3bbc02f17a7f7a81a40b16b8273f6
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2020-07-02 15:38:41 -0500

    Fix gesture detection and debounce gestures

--- src/events.rs
+++ src/events.rs
@@ -1,8 +1,16 @@
+#![allow(unused)]
+
 /// The maximum travel before a tap is considered a swipe, in millimeters.
-const MAX_TAP_DISTANCE: f32 = 2.0f32;
+const MAX_TAP_DISTANCE: f64 = 200f64;
 
 /// The maximum number of tools (fingers) that are tracked and reported on simultaneously.
 const MAX_SLOTS: usize = 5;
+
+/// How long before the event state resets
+const EVENT_TIMEOUT: f64 = 10_593_665_152f64;
+
+/// A new gesture (note: not a new report) will not be entertained in this timespan.
+const DEBOUNCE_TIME: f64 = 0.1f64;
 
 pub(crate) struct EventLoop {
     report: SynReport,
@@ -17,7 +25,7 @@ impl EventLoop {
         }
     }
 
-    pub fn add_event(&mut self, time: f32, event_type: u8, event_code: u16, event_value: i32) {
+    pub fn add_event(&mut self, time: f64, event_type: u8, event_code: u16, event_value: i32) {
         let event_type: EventType = unsafe { std::mem::transmute(event_type) };
         let event_code: EventCode = unsafe { std::mem::transmute(event_code) };
 
@@ -29,13 +37,16 @@ impl EventLoop {
         });
     }
 
-    pub fn update(&mut self) {
-        self.state.update(&self.report)
+    pub fn update(&mut self) -> Option<Gesture> {
+        eprintln!("Processing report with {} events", self.report.events.len());
+        let result = self.state.update(&mut self.report);
+        self.report.events.clear();
+        return result;
     }
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 #[repr(u8)]
 enum EventType {
     /// Unknown
@@ -48,7 +59,7 @@ enum EventType {
 // Until it's proven that the different namespaces can collide (e.g. ABS_* and BTN_* sharing
 // values), just keep them in one enum for our own sanity.
 #[allow(non_camel_case_types)]
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
 #[repr(u16)]
 enum EventCode {
     // Absolute Events (reported per-tool)
@@ -79,7 +90,8 @@ enum EventCode {
     BTN_TOOL_QUADTAP = 335,
 }
 
-enum Direction {
+#[derive(Debug, PartialEq)]
+pub(crate) enum Direction {
     None,
     Up,
     Down,
@@ -87,8 +99,8 @@ enum Direction {
     Right,
 }
 
-#[derive(Clone, Debug)]
-enum Fingers {
+#[derive(Clone, Debug, PartialEq)]
+pub(crate) enum Fingers {
     One = 1,
     Two = 2,
     Three = 3,
@@ -97,9 +109,9 @@ enum Fingers {
 
 // Used to abstract away the event source. In the future, we can migrate from
 // using evtest to reading from the input device directly.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct SynEvent {
-    time: f32,
+    time: f64,
     evt_type: EventType,
     code: EventCode,
     value: i32,
@@ -113,13 +125,11 @@ struct SynReport {
 }
 
 /// A result derived from one or more [`SynReport`] instances in a stream.
-enum Gesture {
+#[derive(Debug)]
+pub(crate) enum Gesture {
     Tap(Fingers),
     Swipe(Fingers, Direction),
 }
-
-/// How long before the event state resets
-const EVENT_TIMEOUT: f32 = 2f32;
 
 #[derive(Debug, Default)]
 struct Position {
@@ -132,8 +142,8 @@ fn pos(x: i32, y: i32) -> Position {
 }
 
 /// Returns the Euclidean distance between two positions
-fn get_distance(pos1: &Position, pos2: &Position) -> f32 {
-    (((pos2.x - pos1.x).pow(2) + (pos2.y - pos1.y).pow(2)) as f32).sqrt()
+fn get_distance(pos1: &Position, pos2: &Position) -> f64 {
+    (((pos2.x - pos1.x).pow(2) + (pos2.y - pos1.y).pow(2)) as f64).sqrt()
 }
 
 fn get_direction(pos1: &Position, pos2: &Position) -> Direction {
@@ -147,9 +157,9 @@ fn get_direction(pos1: &Position, pos2: &Position) -> Direction {
     } else {
         // Interpret as movement along the y-axis only
         if pos2.y > pos1.y {
-            Direction::Up
-        } else {
             Direction::Down
+        } else {
+            Direction::Up
         }
     }
 }
@@ -164,20 +174,21 @@ struct TouchpadState {
     pub slot_states: [Option<SlotState>; MAX_SLOTS],
     pub start_xy: Option<Position>,
     pub end_xy: Option<Position>,
-    pub last_ts: f32,
+    pub last_ts: f64,
+    pub last_gesture_time: f64,
     pub last_finger: Option<Fingers>,
-    pub finger_start: Option<f32>,
-    pub one_finger_duration: f32,
-    pub two_finger_duration: f32,
-    pub three_finger_duration: f32,
-    pub four_finger_duration: f32,
+    pub finger_start: Option<f64>,
+    pub one_finger_duration: f64,
+    pub two_finger_duration: f64,
+    pub three_finger_duration: f64,
+    pub four_finger_duration: f64,
 }
 
 #[derive(Debug, Default)]
 struct SlotState {
     pub complete: bool,
     pub tool_id: Option<i32>,
-    pub last_ts: f32,
+    pub last_ts: f64,
     pub start_xy: Option<Position>,
     pub end_xy: Option<Position>,
 }
@@ -191,7 +202,7 @@ impl SlotState {
         }
     }
 
-    pub fn distance(&self) -> Option<f32> {
+    pub fn distance(&self) -> Option<f64> {
         if let (Some(start_xy), Some(end_xy)) = (&self.start_xy, &self.end_xy) {
             Some(get_distance(start_xy, end_xy))
         } else {
@@ -210,26 +221,34 @@ impl SlotState {
 
 impl TouchpadState {
     pub fn reset(&mut self) {
+        eprintln!("***RESET***");
         self.slot_states = Default::default();
         self.start_xy = None;
         self.end_xy = None;
+        // self.last_gesture_time should not be reset!
+        // self.last_gesture_time = 0f64;
         self.last_finger = None;
         self.finger_start = None;
-        self.one_finger_duration = 0f32;
-        self.two_finger_duration = 0f32;
-        self.three_finger_duration = 0f32;
-        self.four_finger_duration = 0f32;
+        self.one_finger_duration = 0f64;
+        self.two_finger_duration = 0f64;
+        self.three_finger_duration = 0f64;
+        self.four_finger_duration = 0f64;
     }
 
-    pub fn update(&mut self, report: &SynReport) {
+    pub fn update(&mut self, report: &mut SynReport) -> Option<Gesture> {
         let mut reset = false;
         let mut overall_x = None;
         let mut overall_y = None;
 
         // Loop over events and handle each slot separately
         {
+            let prev_finger_start = self.finger_start;
             let mut slot_id = 0usize;
             let mut slot = &mut self.slot_states[0];
+            // A slot id is only specified if more than one tool is detected.
+            if slot.is_none() {
+                *slot = Some(Default::default());
+            }
             let mut slot_x = None;
             let mut slot_y = None;
             for event in &report.events {
@@ -252,7 +271,7 @@ impl TouchpadState {
                         // This just tells us we're using a multitouch-capable trackpad and the
                         // id of the slot that contains information about the tool (finger) being
                         // tracked.
-                        slot_id = event.code as usize;
+                        slot_id = event.value as usize;
                         self.slot_states[slot_id] = Some(Default::default());
                         slot = &mut self.slot_states[slot_id];
                     }
@@ -275,41 +294,68 @@ impl TouchpadState {
 
                     // Finger state applied
                     (EventType::EV_KEY, EventCode::BTN_TOOL_FINGER) if event.value == 1 => {
+                        eprintln!("one finger press");
+                        self.finger_start = Some(event.time);
                         self.last_finger = Some(Fingers::One);
                     }
                     (EventType::EV_KEY, EventCode::BTN_TOOL_DOUBLETAP) if event.value == 1 => {
+                        eprintln!("two finger press");
+                        self.finger_start = Some(event.time);
                         self.last_finger = Some(Fingers::Two);
                     }
                     (EventType::EV_KEY, EventCode::BTN_TOOL_TRIPLETAP) if event.value == 1 => {
+                        self.finger_start = Some(event.time);
                         self.last_finger = Some(Fingers::Three);
                     }
                     (EventType::EV_KEY, EventCode::BTN_TOOL_QUINTTAP) if event.value == 1 => {
+                        self.finger_start = Some(event.time);
                         self.last_finger = Some(Fingers::Four);
                     }
 
                     // Finger state removed
                     // Assuming we never miss an event, the finger should always have started
                     (EventType::EV_KEY, EventCode::BTN_TOOL_FINGER) if event.value == 0 => {
-                        self.one_finger_duration += event.time - self.finger_start.unwrap();
+                        if let Some(finger_start) = prev_finger_start {
+                            eprintln!(
+                                "one finger remove {}",
+                                event.time - prev_finger_start.unwrap()
+                            );
+                            self.one_finger_duration += event.time - prev_finger_start.unwrap();
+                        }
                         self.last_finger = None;
                     }
                     (EventType::EV_KEY, EventCode::BTN_TOOL_DOUBLETAP) if event.value == 0 => {
-                        self.two_finger_duration += event.time - self.finger_start.unwrap();
+                        if let Some(finger_start) = prev_finger_start {
+                            eprintln!(
+                                "two finger remove {}",
+                                event.time - prev_finger_start.unwrap()
+                            );
+                            self.two_finger_duration += event.time - prev_finger_start.unwrap();
+                        }
                         self.last_finger = None;
                     }
                     (EventType::EV_KEY, EventCode::BTN_TOOL_TRIPLETAP) if event.value == 0 => {
-                        self.three_finger_duration += event.time - self.finger_start.unwrap();
+                        if let Some(finger_start) = prev_finger_start {
+                            eprintln!(
+                                "three finger remove {}",
+                                event.time - prev_finger_start.unwrap()
+                            );
+                            self.three_finger_duration += event.time - prev_finger_start.unwrap();
+                        }
                         self.last_finger = None;
                     }
                     (EventType::EV_KEY, EventCode::BTN_TOOL_QUINTTAP) if event.value == 0 => {
-                        self.four_finger_duration += event.time - self.finger_start.unwrap();
+                        if let Some(finger_start) = prev_finger_start {
+                            eprintln!(
+                                "four finger remove {}",
+                                event.time - prev_finger_start.unwrap()
+                            );
+                            self.four_finger_duration += event.time - prev_finger_start.unwrap();
+                        }
                         self.last_finger = None;
                     }
 
                     // Tracking complete event
-                    // XXX: This should be handled on a per-slot basis, allowing fingers to
-                    // drop out in the middle of a gesture and then figuring out only after
-                    // all fingers have been tracked to completion what the deal is.
                     (EventType::EV_ABS, EventCode::ABS_MT_TRACKING_ID) if event.value == -1 => {
                         slot.as_mut().unwrap().complete = true;
                     }
@@ -322,17 +368,22 @@ impl TouchpadState {
 
         if reset {
             self.reset();
-            return;
+            return None;
         }
 
         if let (Some(x), Some(y)) = (overall_x.take(), overall_y.take()) {
             self.push_position(x, y);
         }
 
-        if self.slot_states.iter().all(|slot| slot.is_none() || slot.as_ref().unwrap().complete) {
-            self.process();
-            self.reset();
+        if self.last_finger.is_none() {
+            // if self.slot_states.iter().all(|slot| slot.is_none() || slot.as_ref().unwrap().complete) {
+            if let Some(gesture) = self.process() {
+                self.reset();
+                return Some(gesture);
+            }
         }
+
+        return None;
     }
 
     pub fn push_position(&mut self, x: i32, y: i32) {
@@ -343,7 +394,7 @@ impl TouchpadState {
         }
     }
 
-    fn process(&self) -> Option<Gesture> {
+    fn process(&mut self) -> Option<Gesture> {
         if self.start_xy.is_none() {
             eprintln!("Received report but indeterminate start");
             return None;
@@ -377,19 +428,28 @@ impl TouchpadState {
 
         let distance = match &self.end_xy {
             Some(end_xy) => get_distance(self.start_xy.as_ref().unwrap(), &end_xy),
-            None => 0f32,
+            None => 0f64,
         };
 
-        if distance < MAX_TAP_DISTANCE {
-            Some(Gesture::Tap(finger))
+        eprintln!("Distance: {}", distance);
+
+        dbg!( self.last_ts); dbg!(self.last_gesture_time);
+        if self.last_ts - self.last_gesture_time > DEBOUNCE_TIME {
+            self.last_gesture_time = self.last_ts;
+            if distance < MAX_TAP_DISTANCE {
+                Some(Gesture::Tap(finger))
+            } else {
+                Some(Gesture::Swipe(
+                    finger,
+                    get_direction(
+                        self.start_xy.as_ref().unwrap(),
+                        self.end_xy.as_ref().unwrap(),
+                    ),
+                ))
+            }
         } else {
-            Some(Gesture::Swipe(
-                finger,
-                get_direction(
-                    self.start_xy.as_ref().unwrap(),
-                    self.end_xy.as_ref().unwrap(),
-                ),
-            ))
+            eprintln!("Gesture ignored by debounce");
+            None
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
 mod events;
 
-fn main() {}
+use regex::Regex;
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+
+fn main() {
+    let mut event_loop = events::EventLoop::new();
+
+    let evtest = Command::new("sudo")
+        .args(&["evtest", "/dev/input/event11"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
+
+    // Event: time 1593656931.323635, type 3 (EV_ABS), code 47 (ABS_MT_SLOT), value 0
+    let event_regex =
+        Regex::new(r#"time (\d+\.\d+), type (\d+) .* code (\d+) .* value (\d+)"#).unwrap();
+
+    let reader = BufReader::new(evtest.stdout.unwrap());
+    for line in reader.lines() {
+        let line = match line {
+            Ok(line) => line,
+            Err(_) => break,
+        };
+
+        // Event: time 1593656931.306879, -------------- SYN_REPORT ------------
+        if line.contains("SYN_REPORT") {
+            if let Some(gesture) = event_loop.update() {
+                println!("{:?}", gesture);
+            }
+            continue;
+        }
+
+        if let Some(captures) = event_regex.captures(&line) {
+            let time: f64 = captures[1].parse().unwrap();
+            let event_type: u8 = captures[2].parse().unwrap();
+            let code: u16 = captures[3].parse().unwrap();
+            let value: i32 = captures[4].parse().unwrap();
+
+            eprintln!("{}", line);
+            event_loop.add_event(time, event_type, code, value);
+        }
+    }
+}
